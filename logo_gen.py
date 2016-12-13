84264e266b73d1d32868a93a7049d7b371bced65
ianfitzpatrick1 <ianfitz@gmail.com>
2016-12-12 20:07:09 -0600

    Using twurlrc for authentication

--- logo_gen.py
+++ logo_gen.py
@@ -8,11 +8,22 @@ def draw_center_text(draw_obj, W, H, font_path, font_name, text, y_pos, font_siz
     x = (W - text_x)/2
     draw_obj.text ( (x, y_pos), text, font=font, fill=font_color )
 
+def draw_justify_text(draw_obj, W, H, font_path, font_name, text, y_pos, font_size, font_color, margin=0):
+    font  =  ImageFont.truetype ( font_path + font_name, font_size )    
+    canvas_width = W - (margin * 2)
+    char_x_offset = canvas_width / len(text)
+
+    for idx, char in enumerate(text):
+        char_size = font.getsize(char)[0]
+        x_inner_offset = (char_x_offset - char_size) / 2
+        x = ( char_x_offset * idx ) + margin + x_inner_offset
+        draw_obj.text ( (x, y_pos), char, font=font, fill=font_color )
+
 def get_filename(prefix='tplogo', filetype='png'):
     filename = '%s_%s.%s' % ( prefix, str(uuid.uuid4().fields[-1]), filetype )
     return filename
 
-def gen_logo(basedir, outputdir='logos/'):
+def gen_logo(basedir, outputdir='logos/', just_show_image=False):
     W, H = (400, 250)
     COLOR_BLUE = (0,0,116)
     COLOR_RED = (174,0,4)
@@ -30,7 +41,7 @@ def gen_logo(basedir, outputdir='logos/'):
         'y': 175,
         'size': 26,
         'color': COLOR_RED,
-        'font': 'Berthold_Akzidenz_Grotesk_Bold_Extended.otf'
+        'font': 'Berthold_Akzidenz_Grotesk_Bold_Extended.otf',
     }, {
         'text': 'MAKE AMERICA GREAT AGAIN!',
         'y': 215,
@@ -50,10 +61,20 @@ def gen_logo(basedir, outputdir='logos/'):
     txt = Image.new('RGBA', base.size, (255,255,255,0))
     draw = ImageDraw.Draw(txt)
 
-    for line in message:
-        draw_center_text(draw, W, H, basedir + FONT_PATH, line['font'], line['text'].upper(), line['y'], line['size'], line['color'])
+    # Actual drawing happens here
+    for idx, line in enumerate(message):
+        
+        if idx == 2:
+            draw_center_text(draw, W, H, basedir + FONT_PATH, line['font'], line['text'].upper(), line['y'], line['size'], line['color'])
+        else:
+            draw_justify_text(draw, W, H, basedir + FONT_PATH, line['font'], line['text'].upper(), line['y'], line['size'], line['color'])
+        
 
     out = Image.alpha_composite(base, txt)
-    out.save(img_output)
+    if not just_show_image:
+        out.save(img_output)
+    else:
+        out.show()
+    
     return img_output
     
